4bcab87b1b1d43e71fe9bfb219499daa9d9b9ce9
nekto007 <nekto.korobko@gmail.com>
2020-10-04 00:58:19 +0300

    Настроено логирование через файл конфигурации logging.properties.

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,12 @@
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.LogManager;
+import java.util.logging.Logger;
 
 public class Main {
+    public static final Logger logger = Logger.getLogger(Main.class.getName());
     private static String addCommand = "add Василий Петров " +
             "vasily.petrov@gmail.com +79787775747";
     private static String commandExamples = "\t" + addCommand + "\n" +
@@ -9,27 +15,41 @@ public class Main {
             commandExamples;
     private static String helpText = "Command examples:\n" + commandExamples;
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        InputStream is = Main.class.getResourceAsStream("/logging.properties");
+        LogManager.getLogManager().readConfiguration(is);
+
+        logger.info("Приложение запускается");
+
         Scanner scanner = new Scanner(System.in);
         StudentStorage executor = new StudentStorage();
-        while (true) {
-            String command = scanner.nextLine();
-            String[] tokens = command.split("\\s+", 2);
-            if (tokens[0].equals("add")) {
-                executor.addStudent(tokens[1]);
-            } else if (tokens[0].equals("list")) {
-                executor.listStudent();
-            } else if (tokens[0].equals("get")) {
-                executor.getStudentByName(tokens[1]).toString();
-            } else if (tokens[0].equals("remove")) {
-                executor.removeStudent(tokens[1]);
-            } else if (tokens[0].equals("count")) {
-                System.out.println("There are " + executor.getCount() + " customers");
-            } else if (tokens[0].equals("help")) {
-                System.out.println(helpText);
-            } else {
-                System.out.println(commandError);
+        logger.info("Хранилище создано.");
+        System.out.println(helpText);
+        try {
+            while (true) {
+                String command = scanner.nextLine();
+                String[] tokens = command.split("\\s+", 2);
+                if (tokens[0].equals("add")) {
+                    executor.addStudent(tokens[1]);
+                } else if (tokens[0].equals("list")) {
+                    executor.listStudent();
+                } else if (tokens[0].equals("get")) {
+                    executor.getStudentByName(tokens[1]);
+                } else if (tokens[0].equals("remove")) {
+                    executor.removeStudent(tokens[1]);
+                } else if (tokens[0].equals("count")) {
+                    System.out.println("There are " + executor.getCount() + " customers");
+                } else if (tokens[0].equals("help")) {
+                    System.out.println(helpText);
+                } else {
+                    System.out.println(commandError);
+                }
             }
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.err.println("In main() array tokens[] is out of bound: " + e);
+            logger.log(Level.SEVERE, "In main() array tokens[] is out of bound: " + e);
+        } finally {
+            System.out.println("Приложение завершается");
         }
     }
 }
--- src/StudentStorage.java
+++ src/StudentStorage.java
@@ -1,6 +1,9 @@
 import java.util.HashMap;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class StudentStorage {
+    public static final Logger logger = Logger.getLogger(Main.class.getName());
     private HashMap<String, Student> storage;
 
     public StudentStorage() {
@@ -8,24 +11,48 @@ public class StudentStorage {
     }
 
     public void addStudent(String data) {
-        String[] components = data.split("\\s+");
-        String name = components[0] + " " + components[1];
-        storage.put(name, new Student(name, components[3], components[2]));
+        logger.info("Добавляем студента");
+        try {
+            String[] components = data.split("\\s+");
+            String name = components[0] + " " + components[1];
+            storage.put(name, new Student(name, components[3], components[2]));
+        } catch (ArrayIndexOutOfBoundsException e) {
+            logger.log( Level.SEVERE, "In addStudent() array components[] is out of bound ! ", e );
+            logger.info("addStudent() declined because of exception !");
+        }
     }
 
     public void listStudent() {
+        logger.info("Показываем список студентов.");
         storage.values().forEach(System.out::println);
     }
 
     public void removeStudent(String name) {
-        storage.remove(name);
+        logger.info("Удаляем студента.");
+        try {
+            storage.remove(name);
+        }catch (NullPointerException e) {
+            logger.log( Level.SEVERE, "In removeStudent() parameter name is null ! ", e );
+            logger.info("removeStudent() declined because of exception !");
+        }
+
+
     }
 
     public Student getStudentByName(String name) {
-        return storage.get(name);
+        logger.info("Находим студента по имени.");
+        try {
+            return storage.get(name);
+        }catch (NullPointerException e) {
+            logger.log( Level.SEVERE, "In getStudentByName() parameter name is null: ", e );
+            logger.info("getStudentByName() declined because of exception !");
+            return null;
+        }
+
     }
 
     public int getCount() {
+        logger.info("Выводим количество студентов.");
         return storage.size();
     }
 }
